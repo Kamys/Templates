e294b3253a4cdd0cbe1a9b3c39352b86c7e3d696
Nikita Kamyshenko <nikita.kamyshenko@csssr.com>
2021-11-05 21:11:08 +0300

    Added eureka server

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val exposedVersion = "0.34.1"
 val springVersion = "2.5.6"
+val springCloudVersion = "2020.0.4"
 
 plugins {
-    kotlin("jvm") version "1.5.30"
     application
 
-    id("org.jetbrains.kotlin.plugin.spring") version "1.5.31"
+    kotlin("jvm") version "1.5.30"
+    kotlin("plugin.spring") version "1.5.31"
+
+    id("org.springframework.boot") version "2.5.6"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "com.template"
@@ -26,6 +30,15 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.postgresql:postgresql:42.3.1")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+}
+
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.test {
--- src/main/kotlin/com/template/Application.kt
+++ src/main/kotlin/com/template/Application.kt
@@ -1,15 +1,16 @@
 package com.template
 
-import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@EnableEurekaServer
 class Application
 
 object UserTable : Table() {
@@ -21,11 +22,8 @@ fun main() {
     SpringApplication.run(Application::class.java)
 
     transaction {
-        SchemaUtils.create (UserTable)
-
-        UserTable.insert {
-            it[UserTable.id] = "1"
-            it[UserTable.name] = "Ron"
+        UserTable.selectAll().forEach {
+            println(it[UserTable.name])
         }
     }
 }
